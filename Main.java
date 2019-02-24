a96c8ee842c02c847889dde056fd7c9d8d46accd
Beadsley <danbeadleson@gmail.com>
2019-02-24 13:42:36 +0100

    closes #13

--- Main.java
+++ Main.java
@@ -7,17 +7,6 @@ public class Main{
 		Interaction I1= new Interaction();
 		I1.welcome();
 		
-		//ToDoList t1= new ToDoList();
-		//Task t=new Task("make bed");
-		//t1.addToList(t);
-		//t1.exportFile("/Users/beadsley/Dropbox/IP/Bin/");
-		//ToDoList.importFile("/Users/beadsley/Dropbox/IP/Bin/ToList.txt");
-
-		
-		//Formatter.dateFormatter(java.time.LocalDate.now());
-
-
-
 
 	}
 
